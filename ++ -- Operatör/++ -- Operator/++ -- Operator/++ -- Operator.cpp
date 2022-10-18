cb2f64c88739c67a3e3231762833d89333caf700
Recep Enes <recepenskaya@gmail.com>
2022-10-18 18:21:55 +0300

    Ders

---
+++ ++ -- Operatör/++ -- Operator/++ -- Operator/++ -- Operator.cpp
@@ -0,0 +1,28 @@
+﻿// ++ -- Operator.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int  sayi;
+    sayi = 10;
+    
+    printf("Sayi: %d \n", sayi);
+
+    printf("Sayi: %d \n", ++sayi);
+
+    printf("Sayi: %d \n", sayi);
+
+    printf("Sayi: %d \n", ++sayi);
+
+    printf("Sayi: %d \n", sayi);
+}
+
+//Kısaca atadığınız değişkenin (float,double,int vs.) 
+//Girilen sayı: (x)  .Değişkeninizin önüne ++ veya -- operatörlerinden herhangi birini koyarsanız;
+//İlk girdiyi +1 veya -1 azaltır ve bir sonraki girdi de x+1 veya x-1 olarak gözükür.
+
+//Eğer değişkeninizin arkasına ++ veya -- operatörlerini koyarsanız;
+//İlk girdiniz değişken sayınız gözükür ve bir sonraki girdi x+1 veya x-1 olarak gözükür.
+//Yukarda deneyiniz.
